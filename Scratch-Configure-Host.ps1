50431c7eb3666f1a1f4ad57b9ffcad9ad022c8aa
Bill Curtis <wcurtis@wcurtis.net>
2020-07-28 15:21:07 -0400

    Removed Jumbo Frames settings

--- Scratch-Configure-Host.ps1
+++ Scratch-Configure-Host.ps1
@@ -36,29 +36,5 @@ Write-Verbose "Setting Bandwidth Weight on VM Network Adapters"
 # Set bandwidth weight on VMs
 Get-VM | Set-VMNetworkAdapter -MinimumBandwidthWeight 40
 
-# Set Jumbo Frames on Physical Host
-Write-Verbose "Setting Jumbo Frames on the Physical Host"
-Set-NetAdapterAdvancedProperty -Name $NetworkAdapterNames -RegistryKeyword "*JumboPacket" -RegistryValue 9014
-
-# Set Jumbo Frames on Physical Hosts vNIC
-Write-Verbose "Set Jumbo Frames on Mgmt vNICs"
-$mgmtVNics = Get-Netadapter | ? { $_.Name -match "vEthernet" } | Set-NetAdapterAdvancedProperty -RegistryKeyword "*JumboPacket" -RegistryValue 9014
-
-# Set VM Nics to the correct Jumbo Frames
-Write-Verbose "Setting VMs to the correct Jumbo Frames"
-$runningVMs = Get-VM | Where-Object { $_.State -eq "Running" }
-
-foreach ($runningVM in $runningVMs) {
-
-    Invoke-Command -VMId $runningVM.VMId -ArgumentList $MTUSize -Credential $VMcred -ScriptBlock {
-
-        Write-Host "Setting Jumbo Packets on $env:COMPUTERNAME"
-
-        Get-Netadapter | Set-NetAdapterAdvancedProperty -RegistryKeyword "*JumboPacket" -RegistryValue 9014
-
-    }
-
-}
-
 # Fin
 
